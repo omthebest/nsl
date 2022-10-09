859a21135ed7a7d9ba098508f559988b5b28fcc3
omthebest <100492294+omthebest@users.noreply.github.com>
2022-10-09 15:00:52 -0400

    Add files via upload

---
+++ kwitterroom.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5Ddtq4YGLZNf_GS5aHLa2BIK9hQYD0g4",
+    authDomain: "webchat-7ac52.firebaseapp.com",
+    databaseURL: "https://webchat-7ac52-default-rtdb.firebaseio.com",
+    projectId: "webchat-7ac52",
+    storageBucket: "webchat-7ac52.appspot.com",
+    messagingSenderId: "975408974995",
+    appId: "1:975408974995:web:ca7477614103ca2addd166"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
